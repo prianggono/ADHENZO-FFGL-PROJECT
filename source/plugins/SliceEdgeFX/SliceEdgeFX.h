2083023b77faf00680ab3f0ca5821e024d533645
MediaTeck <rangga.4alone@gmail.com>
2026-09-24 15:54:32 +0700

    Add source/plugins/SliceEdgeFX/SliceEdgeFX.h

---
+++ source/plugins/SliceEdgeFX/SliceEdgeFX.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <FFGLSDK.h>
+
+class SliceEdgeFX : public CFFGLPlugin
+{
+public:
+    SliceEdgeFX();
+    ~SliceEdgeFX() override = default;
+
+    FFResult InitGL(const FFGLViewportStruct* vp) override;
+    FFResult ProcessOpenGL(ProcessOpenGLStruct* pGL) override;
+    FFResult DeInitGL() override;
+
+    FFResult SetFloatParameter(unsigned int index, float value) override;
+    float GetFloatParameter(unsigned int index) override;
+
+private:
+    ffglex::FFGLShader shader;
+    ffglex::FFGLScreenQuad quad;
+
+    float radiusTL;
+    float radiusTR;
+    float radiusBR;
+    float radiusBL;
+
+    float strokeWidth;
+    float strokeOpacity;
+
+    float colorR;
+    float colorG;
+    float colorB;
+
+    float glow;
+    float glowSize;
+
+    float effectMix;
+};
